c9f8ceba29632579612c4b28a02d9da3a24af146
GiaBaorikkei <thainguyengiabao082105@gmail.com>
2024-08-20 00:49:29 +0700

    update

---
+++ session06/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from "@/components/Button"
+
+export default function Dashboard() {
+  return (
+    <div>page
+        <Button></Button>
+    </div>
+  )
+}
---
+++ session06/app/blog/[...slug]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Blog {
+    id: "string"
+}
+
+interface Props {
+    params:any
+}
+export default function Blog(props: Props) {
+    const {params} = props;
+  return (
+    <div>Bạn đang xem blog với đường dẫn: {params.slug}</div>
+  )
+}
